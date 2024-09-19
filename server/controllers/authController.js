4dcdf451a60f44c5b9d445cda85582cac949e703
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-09-19 14:43:54 +0530

    User Model Created

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,5 @@
+import userModel from "../models/userModel.js"
+
 export const signup = async (req, res) => { }
 export const login = async (req, res) => { }
 export const logout = async (req, res) => { }
---
+++ server/models/userModel.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        profilePicture: {
+            type: String,
+            default: ""
+        },
+        bannerImage: {
+            type: String,
+            default: ""
+        },
+        headline: {
+            type: String,
+            default: "ConnectIn User"
+        },
+        location: {
+            type: String,
+            default: ""
+        },
+        about: {
+            type: String,
+            default: ""
+        },
+        skills: [String],
+        experience: [
+            {
+                title: String,
+                startDate: Date,
+                endDate: Date,
+                description: String
+            }
+        ],
+        education: [
+            {
+                school: String,
+                feildOfStudy: String,
+                startYear: Number,
+                endYear: Number
+            }
+        ],
+        connetions: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ]
+
+
+    },
+    { timestamps: true }
+);
+
+const userModel = mongoose.model("User", userSchema)
+export default userModel
