3c3b6f39e75218cd7d12779fc6d4c191ed4c396e
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-09-20 16:46:49 +0530

    Models Created for Post, Connection Request, Notification

---
+++ server/models/connectionRequestModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const connectionRequestSchema = new mongoose.Schema(
+    {
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        recipient: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: ["pending", "accepted", "rejected"],
+            default: "pending",
+        },
+    },
+    { timestamps: true }
+);
+
+const ConnectionRequestModel = mongoose.model("ConnectionRequest", connectionRequestSchema);
+
+export default ConnectionRequestModel;
